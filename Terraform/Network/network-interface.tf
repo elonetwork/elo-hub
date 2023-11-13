8b1f0ffa75ac8f8b503c4a0671318ab2c6114005
DAHANEYassine <dahaneee3@gmail.com>
2023-11-13 20:39:39 +0100

    vnet-hub with infrstructure/Security with terragrunt

--- Terraform/Network/network-interface.tf
+++
@@ -1,7 +0,0 @@
-module "app_interface_bastion" {
-  source = "../Modules/module-interface"
-  name-interface = var.name-interface
-  resource_group_name=var.resource_group_name
-  subnet_id = var.subnet_id
-  location = var.location
-}
--- Terraform/Network/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "subnet-bastion-id" {
-  value = module.app_interface_bastion.app_interface_id
-}
